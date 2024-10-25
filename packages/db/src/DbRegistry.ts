cf9697c14fb65a36eb4a56977c25ff7891c34746
Bruno Zorić <bruno.zoric@gmail.com>
2024-10-25 14:30:57 +0200

    refactor(db): simplify store and fetch of registry items

--- packages/db/src/DbRegistry.ts
+++ packages/db/src/DbRegistry.ts
@@ -2,42 +2,19 @@ import { IRegistry, IRegistryItem, IRegistryRegisterParams } from "./types";
 import { GenericRecord } from "@webiny/api/types";
 
 export class DbRegistry implements IRegistry {
-    private readonly items: IRegistryItem[] = [];
-    private readonly called: GenericRecord<string, number> = {};
+    private readonly items: GenericRecord<string, IRegistryItem> = {};
 
     public register<T = unknown>(input: IRegistryRegisterParams<T>): void {
-        const existing = this.items.some(item => {
-            if (item.app !== input.app) {
-                return false;
-            }
-            for (const tag of input.tags) {
-                if (item.tags.includes(tag)) {
-                    return true;
-                }
-            }
-            const hasAllTags = input.tags.every(tag => {
-                return item.tags.includes(tag);
-            });
-            if (!hasAllTags) {
-                return false;
-            }
-            return item.tags.length === input.tags.length;
-        });
-        const key = `${input.app}-${input.tags.join("-")}`;
-        if (existing) {
-            this.called[key]++;
-            console.log(
-                `Called registration ${input.app} (${input.tags.join(", ")}) #${this.called[key]}`
+        const key = `${input.app}-${input.tags.sort().join("-")}`;
+
+        if (this.items[key]) {
+            throw new Error(
+                `Item with app "${input.app}" and tags "${input.tags.join(
+                    ", "
+                )}" is already registered.`
             );
-            return;
-            // throw new Error(
-            //     `Item with app "${input.app}" and tags "${input.tags.join(
-            //         ", "
-            //     )}" is already registered.`
-            // );
         }
-        this.called[key] = 1;
-        this.items.push(input);
+        this.items[key] = input;
     }
 
     public getOneItem<T = unknown>(cb: (item: IRegistryItem<T>) => boolean): IRegistryItem<T> {
@@ -59,6 +36,14 @@ export class DbRegistry implements IRegistry {
     }
 
     public getItems<T = unknown>(cb: (item: IRegistryItem<T>) => boolean): IRegistryItem<T>[] {
-        return (this.items as IRegistryItem<T>[]).filter(cb);
+        const results: IRegistryItem<T>[] = [];
+        for (const key in this.items) {
+            const item = this.items[key] as IRegistryItem<T>;
+            if (cb(item)) {
+                results.push(item);
+            }
+        }
+
+        return results;
     }
 }
