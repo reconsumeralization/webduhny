7ef60b3d570fe74c7983180ed195b6b7cd96029d
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-11-18 16:02:07 +0100

    fix(app-headless-cms): add BottomInfoBar to app-headless-cms

--- packages/app-headless-cms/src/admin/components/ContentEntries/Table/styled.tsx
+++ packages/app-headless-cms/src/admin/components/ContentEntries/Table/styled.tsx
@@ -1,12 +1,5 @@
 import styled from "@emotion/styled";
-import { css } from "emotion";
 
-import { ListItemGraphic as ListItemGraphicBase } from "@webiny/ui/List";
-
-export const ListItemGraphic = styled(ListItemGraphicBase)`
-    margin-right: 25px;
+export const Container = styled.div`
+    margin-bottom: 31px;
 `;
-
-export const menuStyles = css(`
-    width: 200px;
-`);
