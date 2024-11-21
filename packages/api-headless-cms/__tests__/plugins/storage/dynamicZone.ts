6141f7427e26e869d0a43928e814375304eee19c
Bruno Zorić <bruno.zoric@gmail.com>
2024-11-21 08:59:03 +0100

    fix(api-headless-cms): add missing fields dynamic zone sdl tests

--- packages/api-headless-cms/__tests__/plugins/storage/dynamicZone.ts
+++ packages/api-headless-cms/__tests__/plugins/storage/dynamicZone.ts
@@ -7,7 +7,7 @@ import { PluginsContainer } from "@webiny/plugins";
 
 const field = pageModel.fields.find(f => f.id === "peeeyhtc") as CmsModelDynamicZoneField;
 
-const inputValue = [
+const initialValue = [
     {
         text: "Simple Text #1",
         _templateId: "81qiz2v453wx9uque0gox"
@@ -76,7 +76,76 @@ const inputValue = [
         _templateId: "qi81z2v453wx9uque0gox"
     }
 ];
-const expectedValue = [
+const expectedInitialValue = [
+    {
+        text: "Simple Text #1",
+        _templateId: "81qiz2v453wx9uque0gox"
+    },
+    {
+        title: "Hero Title #1",
+        date: new Date("2024-11-05"),
+        time: "11:05:59",
+        dateTimeWithoutTimezone: new Date("2024-11-05T11:05:59.000Z"),
+        dateTimeWithTimezone: "2024-11-05T11:05:59.000+01:00",
+        _templateId: "cv2zf965v324ivdc7e1vt"
+    },
+    {
+        title: "Hero Title #2",
+        date: new Date("2024-11-05"),
+        time: "11:05:59",
+        dateTimeWithoutTimezone: new Date("2024-11-06T11:05:59.000Z"),
+        dateTimeWithTimezone: "2024-11-06T11:05:59.000+01:00",
+        _templateId: "cv2zf965v324ivdc7e1vt"
+    },
+    {
+        nestedObject: {
+            objectTitle: "Objective title #1",
+            objectNestedObject: [
+                {
+                    nestedObjectNestedTitle: "Content Objecting nested title #1",
+                    date: new Date("2024-11-05"),
+                    time: "11:05:59",
+                    dateTimeWithoutTimezone: new Date("2024-11-05T11:05:59.000Z"),
+                    dateTimeWithTimezone: "2024-11-05T11:05:59.000+01:00"
+                },
+                {
+                    nestedObjectNestedTitle: "Content Objecting nested title #2",
+                    date: new Date("2024-11-05"),
+                    time: "11:05:59",
+                    dateTimeWithoutTimezone: new Date("2024-11-06T11:05:59.000Z"),
+                    dateTimeWithTimezone: "2024-11-06T11:05:59.000+01:00"
+                }
+            ]
+        },
+        dynamicZone: {
+            authors: [
+                {
+                    id: "john-doe#0001",
+                    entryId: "john-doe",
+                    modelId: "author"
+                }
+            ],
+            _templateId: "0emukbsvmzpozx2lzk883"
+        },
+        _templateId: "9ht43gurhegkbdfsaafyads"
+    },
+    {
+        author: {
+            id: "john-doe#0001",
+            entryId: "john-doe",
+            modelId: "author"
+        },
+        authors: [
+            {
+                id: "john-doe#0001",
+                entryId: "john-doe",
+                modelId: "author"
+            }
+        ],
+        _templateId: "qi81z2v453wx9uque0gox"
+    }
+];
+const expectedToStorageValue = [
     {
         text: "Simple Text #1",
         _templateId: "81qiz2v453wx9uque0gox"
@@ -156,12 +225,31 @@ describe("dynamic zone storage transform", () => {
     it("should properly transform data to storage", async () => {
         const result = await plugin.toStorage({
             field,
-            value: inputValue,
+            value: initialValue,
             getStoragePlugin,
             model: pageModel as CmsModel,
             plugins
         });
 
-        expect(result).toEqual(expectedValue);
+        expect(result).toEqual(expectedToStorageValue);
+    });
+
+    it("should transform data from storage", async () => {
+        const input = await plugin.toStorage({
+            field,
+            value: initialValue,
+            getStoragePlugin,
+            model: pageModel as CmsModel,
+            plugins
+        });
+
+        const result = await plugin.fromStorage({
+            field,
+            value: input,
+            getStoragePlugin,
+            model: pageModel as CmsModel,
+            plugins
+        });
+        expect(result).toEqual(expectedInitialValue);
     });
 });
--- packages/api-headless-cms/src/storage/dynamicZone.ts
+++ packages/api-headless-cms/src/storage/dynamicZone.ts
@@ -5,7 +5,7 @@ import { CmsModel, CmsModelDynamicZoneField, CmsModelField } from "~/types";
 import { PluginsContainer } from "@webiny/plugins";
 import pReduce from "p-reduce";
 
-interface IProcessFromStorageParams {
+interface IProcessParams {
     model: CmsModel;
     field: CmsModelDynamicZoneField;
     value: GenericRecord;
@@ -13,7 +13,7 @@ interface IProcessFromStorageParams {
     plugins: PluginsContainer;
 }
 
-const processToStorage = async (params: IProcessFromStorageParams): Promise<GenericRecord> => {
+const processToStorage = async (params: IProcessParams): Promise<GenericRecord> => {
     const { model, field: parentField, value: input, getStoragePlugin, plugins } = params;
 
     const output: GenericRecord = structuredClone(input);
@@ -32,7 +32,7 @@ const processToStorage = async (params: IProcessFromStorageParams): Promise<Gene
             const value = values[field.fieldId];
 
             if (!value) {
-                delete values[field.fieldId];
+                values[field.fieldId] = value;
                 return values;
             }
             const plugin = getStoragePlugin(field.type);
@@ -55,19 +55,60 @@ const processToStorage = async (params: IProcessFromStorageParams): Promise<Gene
     );
 };
 
+const processFromStorage = async (params: IProcessParams): Promise<GenericRecord> => {
+    const { model, field: parentField, value: input, getStoragePlugin, plugins } = params;
+
+    const output: GenericRecord = structuredClone(input);
+
+    if (!output._templateId) {
+        return output;
+    }
+    const template = parentField.settings.templates.find(t => t.id === output._templateId);
+    if (!template || !template.fields.length) {
+        return output;
+    }
+
+    return await pReduce(
+        template.fields,
+        async (values, field) => {
+            const value = values[field.fieldId];
+
+            if (!value) {
+                return values;
+            }
+            const plugin = getStoragePlugin(field.type);
+            if (!plugin) {
+                console.error(`Missing storage plugin for field type "${field.type}".`);
+                delete values[field.fieldId];
+                return values;
+            }
+            values[field.fieldId] = await plugin.fromStorage({
+                plugins,
+                getStoragePlugin,
+                model,
+                field,
+                value
+            });
+
+            return values;
+        },
+        output
+    );
+};
+
 export const createDynamicZoneStorageTransform = (): StorageTransformPlugin => {
     return new StorageTransformPlugin({
         name: "headless-cms.storage-transform.dynamicZone.default",
         fieldType: "dynamicZone",
         toStorage: async ({ field, value, getStoragePlugin, model, plugins }) => {
             if (!value) {
-                return null;
+                return value;
             } else if (field.multipleValues) {
                 if (!Array.isArray(value)) {
                     return value;
                 }
                 return await pMap(value as GenericRecord[], async value => {
-                    return processToStorage({
+                    return await processToStorage({
                         model,
                         field: field as CmsModelDynamicZoneField,
                         value,
@@ -76,7 +117,7 @@ export const createDynamicZoneStorageTransform = (): StorageTransformPlugin => {
                     });
                 });
             }
-            return processToStorage({
+            return await processToStorage({
                 model,
                 field: field as CmsModelDynamicZoneField,
                 value,
@@ -84,8 +125,30 @@ export const createDynamicZoneStorageTransform = (): StorageTransformPlugin => {
                 plugins
             });
         },
-        fromStorage: async ({ value }) => {
-            return value;
+        fromStorage: async ({ field, value, getStoragePlugin, model, plugins }) => {
+            if (!value) {
+                return value;
+            } else if (field.multipleValues) {
+                if (!Array.isArray(value)) {
+                    return value;
+                }
+                return await pMap(value as GenericRecord[], async value => {
+                    return await processFromStorage({
+                        model,
+                        field: field as CmsModelDynamicZoneField,
+                        value,
+                        getStoragePlugin,
+                        plugins
+                    });
+                });
+            }
+            return await processFromStorage({
+                model,
+                field: field as CmsModelDynamicZoneField,
+                value,
+                getStoragePlugin,
+                plugins
+            });
         }
     });
 };
