954e496914aebc4fc4813732d07cc337aa324eab
Bruno Zorić <bruno.zoric@gmail.com>
2024-11-26 11:06:12 +0100

    chore(pulumi-sdk): update pulumi

--- packages/pulumi-aws/src/constants.ts
+++ packages/pulumi-aws/src/constants.ts
@@ -1,9 +1,5 @@
 import { lambda } from "@pulumi/aws";
 
-/**
- * TODO change when pulumi updates
- */
-// export const LAMBDA_RUNTIME = lambda.Runtime.NodeJS20dX;
-export const LAMBDA_RUNTIME = "nodejs22.x";
+export const LAMBDA_RUNTIME = lambda.Runtime.NodeJS20dX;
 
 export const DEFAULT_PROD_ENV_NAMES = ["prod", "production"];
