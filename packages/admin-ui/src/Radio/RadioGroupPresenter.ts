d27dcad39c32428d60b682d42b723c3932051fb4
Leonardo Giacone <leonardo.giacone@gmail.com>
2024-11-28 09:01:53 +0100

    chore: change RadioItemFormatter

--- packages/admin-ui/src/Radio/RadioGroupPresenter.ts
+++ packages/admin-ui/src/Radio/RadioGroupPresenter.ts
@@ -1,8 +1,8 @@
 import { makeAutoObservable } from "mobx";
 import { RadioGroupVm } from "./RadioGroup";
-import { RadioItem } from "~/Radio/RadioItem";
-import { RadioItemMapper } from "~/Radio/RadioItemMapper";
-import { RadioItemDto } from "~/Radio/RadioItemDto";
+import { RadioItem } from "./RadioItem";
+import { RadioItemFormatter } from "./RadioItemFormatter";
+import { RadioItemDto } from "./RadioItemDto";
 
 interface RadioGroupPresenterParams<TValue = string> {
     items: RadioItemDto[];
@@ -28,7 +28,7 @@ class RadioGroupPresenter<TValue = string> implements IRadioGroupPresenter<TValu
 
     get vm() {
         return {
-            items: this.items.map(item => RadioItemMapper.toFormatted(item))
+            items: this.items.map(item => RadioItemFormatter.format(item))
         };
     }
 
