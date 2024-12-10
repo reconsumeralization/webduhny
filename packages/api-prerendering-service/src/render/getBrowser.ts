ebb873e05cec1469b2354153fb9da1ce9132d65a
adrians5j <adrian@webiny.com>
2024-12-10 16:13:07 +0100

    fix: use `ls-string` compression

--- packages/api-prerendering-service/src/render/getBrowser.ts
+++ packages/api-prerendering-service/src/render/getBrowser.ts
@@ -3,12 +3,12 @@
  */
 export const getBrowser = async () => {
     if (process.env["WEBINY_WATCH_LOCAL_INVOCATION"]) {
-        const { default: puppeteer } = await import(/* puppeteer */"puppeteer");
+        const { default: puppeteer } = await import(/* webpackChunkName: "puppeteer" */"puppeteer");
         return puppeteer.launch({});
     }
 
-    const { default: chromium } = await import(/* sparticuz-chromium */ "@sparticuz/chromium");
-    const { default: puppeteer } = await import(/* puppeteer-core */ "puppeteer-core");
+    const { default: chromium } = await import(/* webpackChunkName: "sparticuz-chromium" */ "@sparticuz/chromium");
+    const { default: puppeteer } = await import(/* webpackChunkName: "puppeteer-core" */ "puppeteer-core");
 
     return puppeteer.launch({
         args: chromium.args,
