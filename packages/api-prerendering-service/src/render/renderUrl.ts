6db1fb1f843aec68ef25ef189cd93ee3677451fb
adrians5j <adrian@webiny.com>
2024-12-12 16:32:49 +0100

    chore:undo change

--- packages/api-prerendering-service/src/render/renderUrl.ts
+++ packages/api-prerendering-service/src/render/renderUrl.ts
@@ -1,8 +1,7 @@
+import chromium from "@sparticuz/chromium";
+import puppeteer, { Browser, Page } from "puppeteer-core";
 import posthtml from "posthtml";
 import { noopener } from "posthtml-noopener";
-import type { Page } from "puppeteer";
-import { getBrowser } from "./getBrowser";
-
 /**
  * Package posthtml-plugin-link-preload has no types.
  */
@@ -126,10 +125,18 @@ export const defaultRenderUrlFunction = async (
     url: string,
     params: RenderUrlCallableParams
 ): Promise<RenderResult> => {
-    const browser = await getBrowser();
+    let browser!: Browser;
 
     try {
-        const browserPage = (await browser.newPage()) as Page;
+        browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true
+        });
+
+        const browserPage = await browser.newPage();
 
         // Can be used to add additional logic - e.g. skip a GraphQL query to be made when in pre-rendering process.
         windowSet(browserPage, "__PS_RENDER__", true);
