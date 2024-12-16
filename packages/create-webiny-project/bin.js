16d786cd45e8f7afdd03da1fe55157b7510f4a03
adrians5j <adrian@webiny.com>
2024-12-16 09:59:24 +0100

    wip

--- packages/create-webiny-project/bin.js
+++ packages/create-webiny-project/bin.js
@@ -1,24 +1,26 @@
 #!/usr/bin/env node
 "use strict";
 
-const semver = require("semver");
 const chalk = require("chalk");
-const getYarnVersion = require("./utils/getYarnVersion");
-const getNpmVersion = require("./utils/getNpmVersion");
 const verifyConfig = require("./utils/verifyConfig");
+const { SystemRequirements } = require("@webiny/system-requirements");
 
 (async () => {
-    const minNpmVersion = "10";
-    const minYarnVersion = "1.22.21";
-    /**
-     * Node
-     */
-    const nodeVersion = process.versions.node;
-    if (!semver.satisfies(nodeVersion, `^18 || ^20`)) {
+    const systemRequirements = SystemRequirements.validate();
+
+    if (systemRequirements.valid) {
+        // Verify `.webiny` config file and continue.
+        await verifyConfig();
+
+        require("./index");
+    }
+
+    // Check Node.js version.
+    if (!systemRequirements.node.valid) {
         console.error(
             chalk.red(
                 [
-                    `You are running Node.js ${nodeVersion}, but Webiny requires version 18 or 20.`,
+                    `You are running Node.js ${systemRequirements.node.current}, but Webiny requires version ${systemRequirements.node.requiredVersion}.`,
                     `Please switch to one of the required versions and try again.`,
                     "For more information, please visit https://www.webiny.com/docs/get-started/install-webiny#prerequisites."
                 ].join(" ")
@@ -26,62 +28,32 @@ const verifyConfig = require("./utils/verifyConfig");
         );
         process.exit(1);
     }
-    /**
-     * npm
-     */
-    try {
-        const npmVersion = await getNpmVersion();
-        if (!semver.satisfies(npmVersion, `>=${minNpmVersion}`)) {
-            console.error(
-                chalk.red(
-                    [
-                        `Webiny requires npm@^${minNpmVersion} or higher.`,
-                        `Please run ${chalk.green(
-                            "npm install npm@latest -g"
-                        )}, to get the latest version.`
-                    ].join("\n")
-                )
-            );
-            process.exit(1);
-        }
-    } catch (err) {
-        console.error(chalk.red(`Webiny depends on "npm".`));
 
-        console.log(
-            `Please visit https://docs.npmjs.com/try-the-latest-stable-version-of-npm to install ${chalk.green(
-                "npm"
-            )}.`
-        );
-
-        process.exit(1);
-    }
-
-    /**
-     * yarn
-     */
-    try {
-        const yarnVersion = await getYarnVersion();
-        if (!semver.satisfies(yarnVersion, `>=${minYarnVersion}`)) {
-            console.error(
-                chalk.red(
-                    [
-                        `Webiny requires yarn@^${minYarnVersion} or higher.`,
-                        `Please visit https://yarnpkg.com/ to install ${chalk.green("yarn")}.`
-                    ].join("\n")
-                )
-            );
-            process.exit(1);
-        }
-    } catch (err) {
+    // Check NPM version.
+    if (!systemRequirements.npm.valid) {
         console.error(
-            chalk.red(`Webiny depends on "yarn" and its built-in support for workspaces.`)
+            chalk.red(
+                [
+                    `Webiny requires npm@^${systemRequirements.npm.requiredVersion}.`,
+                    `Please run ${chalk.green(
+                        "npm install npm@latest -g"
+                    )}, to get the latest version.`
+                ].join("\n")
+            )
         );
-
-        console.log(`Please visit https://yarnpkg.com/ to install ${chalk.green("yarn")}.`);
-
         process.exit(1);
     }
 
-    await verifyConfig();
-    require("./index");
+    // Check Yarn version.
+    if (!systemRequirements.yarn.valid) {
+        console.error(
+            chalk.red(
+                [
+                    `Webiny requires yarn@^${systemRequirements.yarn.requiredVersion}.`,
+                    `Please visit https://yarnpkg.com/ to install ${chalk.green("yarn")}.`
+                ].join("\n")
+            )
+        );
+        process.exit(1);
+    }
 })();
