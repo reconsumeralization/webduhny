ad964ac22b01022d73aea8b074fa957cec24844b
Pavel Denisjuk <pavel@webiny.com>
2024-12-17 14:39:34 +0100

    fix(app-website): decorate lexical renderers on first mount

--- packages/app-page-builder/src/render/PageBuilder.tsx
+++ packages/app-page-builder/src/render/PageBuilder.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { AddButtonClickHandlers } from "~/elementDecorators/AddButtonClickHandlers";
 import { AddButtonLinkComponent } from "~/elementDecorators/AddButtonLinkComponent";
 import { InjectElementVariables } from "~/render/variables/InjectElementVariables";
-import { LexicalParagraphRenderer } from "~/render/plugins/elements/paragraph/LexicalParagraph";
-import { LexicalHeadingRenderer } from "~/render/plugins/elements/heading/LexicalHeading";
 
-export const PageBuilder = () => {
+export const PageBuilder = React.memo(() => {
     return (
         <>
             <AddButtonLinkComponent />
             <AddButtonClickHandlers />
             <InjectElementVariables />
-            <LexicalParagraphRenderer />
-            <LexicalHeadingRenderer />
         </>
     );
-};
+});
+
+PageBuilder.displayName = "PageBuilder";
