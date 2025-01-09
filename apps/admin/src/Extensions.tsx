c6ede836139b9afe0481c2e7919dd981ee683dd5
adrians5j <adrian@webiny.com>
2025-01-09 13:25:21 +0100

    wip

--- apps/admin/src/Extensions.tsx
+++ apps/admin/src/Extensions.tsx
@@ -1,6 +1,11 @@
 // This file is automatically updated via scaffolding utilities.
 import React from "react";
+import { Extension as MyCustomExtensionExtension } from "my-custom-extension";
 
 export const Extensions = () => {
-    return <></>;
+    return (
+        <>
+            <MyCustomExtensionExtension />
+        </>
+    );
 };
--- packages/admin-ui/src/AdminTheme/types.ts
+++ packages/admin-ui/src/AdminTheme/types.ts
@@ -1,7 +1,7 @@
-import { COLOR_KEYS, COLOR_PALLETS, SHADES_LEVELS } from "./consts";
+import { COLOR_PALLETS, SHADES_LEVELS } from "./consts";
 
-export type ColorKeys = (typeof COLOR_KEYS)[number];
 export type ColorPalette = (typeof COLOR_PALLETS)[number];
 export type ShadeLevel = (typeof SHADES_LEVELS)[number];
+export type ShadesObject = Record<ShadeLevel, string>;
 
-export type ColorsObject = Record<ColorKeys, string>;
+export type ColorsObject = Record<ColorPalette, string | ShadesObject>;
