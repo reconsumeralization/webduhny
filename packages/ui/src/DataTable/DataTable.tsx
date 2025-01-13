6e69fa8236af18bb1f905eb7027bc23110b296e7
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-01-13 11:34:51 +0100

    docs: add deprecated

--- packages/ui/src/DataTable/DataTable.tsx
+++ packages/ui/src/DataTable/DataTable.tsx
@@ -1,15 +1,63 @@
 import React from "react";
 import {
     DataTable as AdminDataTable,
+    DataTableColumn,
+    DataTableColumns,
+    DataTableColumnSort,
+    DataTableColumnVisibility,
     DataTableDefaultData,
     DataTableProps as AdminDataTableProps,
+    DataTableRow,
     DataTableSorting,
+    OnDataTableColumnVisibilityChange,
     OnDataTableSortingChange
 } from "@webiny/admin-ui";
 
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type Column<T> = DataTableColumn<T>;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type ColumnSort = DataTableColumnSort;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type ColumnVisibility = DataTableColumnVisibility;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type Columns<T> = DataTableColumns<T>;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
 type DefaultData = DataTableDefaultData;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type OnColumnVisibilityChange = OnDataTableColumnVisibilityChange;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
 type OnSortingChange = OnDataTableSortingChange;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
 type Sorting = DataTableSorting;
+/**
+ * @deprecated @webiny/ui package is deprecated and will be removed in future releases.
+ * Please check `DataTable` types from the `@webiny/admin-ui` package instead.
+ */
+type TableRow<T> = DataTableRow<T>;
 
 interface DataTableProps<TEntry>
     extends Omit<AdminDataTableProps<TEntry>, "loading" | "stickyHeader"> {
@@ -29,4 +77,15 @@ const DataTable = <T extends Record<string, any> & DataTableDefaultData>({
     return <AdminDataTable {...props} loading={loadingInitial} stickyHeader={!!stickyHeader} />;
 };
 
-export { DataTable, type DefaultData, type OnSortingChange, type Sorting };
+export {
+    DataTable,
+    type Column,
+    type ColumnSort,
+    type ColumnVisibility,
+    type Columns,
+    type DefaultData,
+    type OnColumnVisibilityChange,
+    type OnSortingChange,
+    type Sorting,
+    type TableRow
+};
