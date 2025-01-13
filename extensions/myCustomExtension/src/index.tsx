afb2d2571b5742c2a3c90cb7fd9c435f619178c3
Pavel Denisjuk <pavel@webiny.com>
2025-01-13 20:44:38 +0100

    wip: switch primary color based on tenant

--- extensions/myCustomExtension/src/index.tsx
+++ extensions/myCustomExtension/src/index.tsx
@@ -1,37 +1,29 @@
 // extensions/adminTheme/src/index.tsx
 import React from "react";
 import { AdminConfig, Plugin } from "@webiny/app-admin";
-import { useSecurity } from "@webiny/app-serverless-cms";
-// import { IsNotRootTenant, IsRootTenant } from "@webiny/app-tenant-manager";
-// import { useSecurity } from "@webiny/app-serverless-cms";
+import { IsNotRootTenant, IsRootTenant } from "@webiny/app-tenant-manager";
 
 const { Theme } = AdminConfig;
 
 const SomethingWithSecurityMustBeChildOfPlugin = () => {
-    console.log("Try using security");
-    const sec = useSecurity();
-    console.log(sec)
-    return null;
-
-    // const sec = useSecurity();
-    // return (
-    //     <>
-    //         <IsRootTenant>
-    //             <AdminConfig>
-    //                 <Theme>
-    //                     <Theme.Color palette={"primary"} color={"red"} />
-    //                 </Theme>
-    //             </AdminConfig>
-    //         </IsRootTenant>
-    //         <IsNotRootTenant>
-    //             <AdminConfig>
-    //                 <Theme>
-    //                     <Theme.Color palette={"primary"} color={"blue"} />
-    //                 </Theme>
-    //             </AdminConfig>
-    //         </IsNotRootTenant>
-    //     </>
-    // );
+    return (
+        <>
+            <IsRootTenant>
+                <AdminConfig>
+                    <Theme>
+                        <Theme.Color palette={"primary"} color={"red"} />
+                    </Theme>
+                </AdminConfig>
+            </IsRootTenant>
+            <IsNotRootTenant>
+                <AdminConfig>
+                    <Theme>
+                        <Theme.Color palette={"primary"} color={"blue"} />
+                    </Theme>
+                </AdminConfig>
+            </IsNotRootTenant>
+        </>
+    );
 };
 
 export const Extension = () => {
