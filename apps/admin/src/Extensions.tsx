d1382319f0b6f26f167874f8c0834271f099c8fc
adrians5j <adrian@webiny.com>
2025-01-15 21:42:36 +0100

    wip

--- apps/admin/src/Extensions.tsx
+++ apps/admin/src/Extensions.tsx
@@ -2,12 +2,10 @@
 // Learn more about extensions: https://webiny.link/extensions
 import React from "react";
 import { Extension as MyCustomExtensionExtension } from "my-custom-extension";
-import { Extension as AdminLogoExtension } from "admin-logo";
 
 export const Extensions = () => {
     return (
         <>
-            <AdminLogoExtension />
             <MyCustomExtensionExtension />
         </>
     );
