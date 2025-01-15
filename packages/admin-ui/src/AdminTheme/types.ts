84bb6166910ea8c8b76a8be49ec42131a974b147
adrians5j <adrian@webiny.com>
2025-01-15 21:36:14 +0100

    wip

--- packages/admin-ui/src/AdminTheme/types.ts
+++
@@ -1,7 +0,0 @@
-import { COLOR_PALLETS, SHADES_LEVELS } from "./consts";
-
-export type ColorPalette = (typeof COLOR_PALLETS)[number];
-export type ShadeLevel = (typeof SHADES_LEVELS)[number];
-export type ShadesObject = Record<ShadeLevel, string>;
-
-export type ColorsObject = Record<ColorPalette, string | ShadesObject>;
