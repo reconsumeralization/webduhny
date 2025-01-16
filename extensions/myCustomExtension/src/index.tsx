239dd56f74568424369d9e11c9410ddda3d3e169
adrians5j <adrian@webiny.com>
2025-01-16 05:57:11 +0100

    wip

--- extensions/myCustomExtension/src/index.tsx
+++ extensions/myCustomExtension/src/index.tsx
@@ -1,35 +1,15 @@
 // extensions/adminTheme/src/index.tsx
 import React from "react";
-import { AdminConfig, Plugin, IsNotRootTenant, IsRootTenant } from "@webiny/app-serverless-cms";
+import { AdminConfig } from "@webiny/app-serverless-cms";
 
 const { Theme } = AdminConfig;
 
-const SomethingWithSecurityMustBeChildOfPlugin = () => {
-    return (
-        <>
-            <IsRootTenant>
-                <AdminConfig>
-                    <Theme>
-                        <Theme.Color palette={"primary"} color={"purple"} />
-                    </Theme>
-                </AdminConfig>
-            </IsRootTenant>
-            <IsNotRootTenant>
-                <AdminConfig>
-                    <Theme>
-                        <Theme.Color palette={"primary"} color={"blue"} />
-                    </Theme>
-                </AdminConfig>
-            </IsNotRootTenant>
-        </>
-    );
-};
-
 export const Extension = () => {
     return (
-        <Plugin>
-            {/* <Plugin> element makes its children render here: `packages/app/src/App.tsx:144` */}
-            <SomethingWithSecurityMustBeChildOfPlugin />
-        </Plugin>
+        <AdminConfig>
+            <Theme>
+                <Theme.Color palette={"primary"} color={"purple"} />
+            </Theme>
+        </AdminConfig>
     );
 };
