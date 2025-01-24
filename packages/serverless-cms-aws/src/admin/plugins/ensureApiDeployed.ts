3bdb224bd44eefc50412f5f225992bec3f3db83e
adrians5j <adrian@webiny.com>
2025-01-24 13:20:48 +0100

    fix: allow skipping of deployment checks

--- packages/serverless-cms-aws/src/admin/plugins/ensureApiDeployed.ts
+++ packages/serverless-cms-aws/src/admin/plugins/ensureApiDeployed.ts
@@ -1,8 +1,20 @@
 import { getStackOutput } from "@webiny/cli-plugin-deploy-pulumi/utils";
-import { createBeforeBuildPlugin } from "@webiny/cli-plugin-deploy-pulumi/plugins";
+import {
+    createBeforeBuildPlugin,
+    createBeforeWatchPlugin
+} from "@webiny/cli-plugin-deploy-pulumi/plugins";
 import { GracefulError } from "@webiny/cli-plugin-deploy-pulumi/utils";
+import type { Callable } from "@webiny/cli-plugin-deploy-pulumi/plugins/PulumiCommandLifecycleEventHookPlugin";
 
-export const ensureApiDeployedBeforeBuild = createBeforeBuildPlugin(({ env }, ctx) => {
+const NO_DEPLOYMENT_CHECKS_FLAG_NAME = "--no-deployment-checks";
+
+const createPluginCallable: (command: "build" | "watch") => Callable = command => (params, ctx) => {
+    const { env, inputs } = params;
+    // Just in case, we want to allow users to skip the system requirements check.
+    if (inputs.deploymentChecks === false) {
+        return;
+    }
+
     const output = getStackOutput({ folder: "apps/api", env });
     const apiDeployed = output && Object.keys(output).length > 0;
     if (apiDeployed) {
@@ -12,14 +24,22 @@ export const ensureApiDeployedBeforeBuild = createBeforeBuildPlugin(({ env }, ct
     const apiAppName = ctx.error.hl("API");
     const adminAppName = ctx.error.hl("Admin");
     const cmd = ctx.error.hl(`yarn webiny deploy api --env ${env}`);
-    ctx.error(`Cannot build ${adminAppName} project application before deploying ${apiAppName}.`);
-
-    throw new GracefulError(
-        [
-            `Before building ${adminAppName} project application, please`,
-            `deploy ${apiAppName} first by running: ${cmd}.`
-        ].join(" ")
+    ctx.error(
+        `Cannot ${command} ${adminAppName} project application before deploying ${apiAppName}.`
     );
-});
 
-ensureApiDeployedBeforeBuild.name = "admin.before-build.ensure-api-deployed";
+    const message = [
+        `Before ${command}ing ${adminAppName} project application, please`,
+        `deploy ${apiAppName} first by running: ${cmd}.`,
+        `If you think this is a mistake, you can also try skipping`,
+        `the deployment checks by appending the ${ctx.error.hl(NO_DEPLOYMENT_CHECKS_FLAG_NAME)} flag.`
+    ];
+
+    throw new GracefulError(message.join(" "));
+};
+
+export const ensureApiDeployedBeforeBuild = createBeforeBuildPlugin(createPluginCallable("build"));
+ensureApiDeployedBeforeBuild.name = "admin.before-deploy.ensure-api-deployed";
+
+export const ensureApiDeployedBeforeWatch = createBeforeWatchPlugin(createPluginCallable("watch"));
+ensureApiDeployedBeforeWatch.name = "admin.before-watch.ensure-api-deployed";
--- packages/serverless-cms-aws/src/enterprise/createAdminApp.ts
+++ packages/serverless-cms-aws/src/enterprise/createAdminApp.ts
@@ -1,14 +1,18 @@
 import { createAdminPulumiApp, CreateAdminPulumiAppParams } from "@webiny/pulumi-aws/enterprise";
 import { uploadAppToS3 } from "~/react/plugins";
 import { PluginCollection } from "@webiny/plugins/types";
-import { ensureApiDeployedBeforeBuild } from "~/website/plugins";
+import { ensureApiDeployedBeforeBuild, ensureApiDeployedBeforeWatch } from "~/admin/plugins";
 
 export interface CreateAdminAppParams extends CreateAdminPulumiAppParams {
     plugins?: PluginCollection;
 }
 
 export function createAdminApp(projectAppParams: CreateAdminAppParams = {}) {
-    const builtInPlugins = [uploadAppToS3({ folder: "apps/admin" }), ensureApiDeployedBeforeBuild];
+    const builtInPlugins = [
+        uploadAppToS3({ folder: "apps/admin" }),
+        ensureApiDeployedBeforeBuild,
+        ensureApiDeployedBeforeWatch
+    ];
 
     const customPlugins = projectAppParams.plugins ? [...projectAppParams.plugins] : [];
 
