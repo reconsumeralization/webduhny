a11d9fe88475f0411c6a2c50427b1523585c0b84
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-02-05 12:47:13 +0100

    chore: deps

--- packages/admin-ui/src/IconPicker/IconPicker.stories.tsx
+++ packages/admin-ui/src/IconPicker/IconPicker.stories.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { IconDto, IconPicker } from "~/IconPicker";
-import { IconName, library } from "@fortawesome/fontawesome-svg-core";
-import { IconPrefix } from "@fortawesome/fontawesome-common-types";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { IconPicker } from "~/IconPicker";
 
 const meta: Meta<typeof IconPicker> = {
     title: "Components/Form/IconPicker",
@@ -21,36 +18,63 @@ const meta: Meta<typeof IconPicker> = {
     }
 };
 
-interface Icons {
-    definitions: Record<IconPrefix, Record<IconName, string[]>>;
-}
-
-const getIcons = () => {
-    const icons: IconDto[] = [];
-    // @ts-expect-error
-    library.add(fas);
-    const definitions = (library as unknown as Icons).definitions;
-    // @ts-expect-error
-    Object.keys(definitions).forEach((pack: IconPrefix) => {
-        const defs = definitions[pack];
-        // @ts-expect-error
-        Object.keys(defs).forEach((icon: IconName) => {
-            icons.push({
-                prefix: pack,
-                name: icon
-            });
-        });
-    });
-
-    return icons;
-};
-
 export default meta;
 type Story = StoryObj<typeof IconPicker>;
 
 export const Default: Story = {
     args: {
-        icons: getIcons()
+        icons: [
+            { prefix: "fas", name: "trash-restore-alt" },
+            { prefix: "fas", name: "trash-can-arrow-up" },
+            { prefix: "fas", name: "naira-sign" },
+            { prefix: "fas", name: "cart-arrow-down" },
+            { prefix: "fas", name: "walkie-talkie" },
+            { prefix: "fas", name: "file-edit" },
+            { prefix: "fas", name: "file-pen" },
+            { prefix: "fas", name: "receipt" },
+            { prefix: "fas", name: "pen-square" },
+            { prefix: "fas", name: "pencil-square" },
+            { prefix: "fas", name: "square-pen" },
+            { prefix: "fas", name: "suitcase-rolling" },
+            { prefix: "fas", name: "person-circle-exclamation" },
+            { prefix: "fas", name: "chevron-down" },
+            { prefix: "fas", name: "battery" },
+            { prefix: "fas", name: "battery-5" },
+            { prefix: "fas", name: "battery-full" },
+            { prefix: "fas", name: "skull-crossbones" },
+            { prefix: "fas", name: "code-compare" },
+            { prefix: "fas", name: "list-dots" },
+            { prefix: "fas", name: "list-ul" },
+            { prefix: "fas", name: "school-lock" },
+            { prefix: "fas", name: "tower-cell" },
+            { prefix: "fas", name: "long-arrow-alt-down" },
+            { prefix: "fas", name: "down-long" },
+            { prefix: "fas", name: "ranking-star" },
+            { prefix: "fas", name: "chess-king" },
+            { prefix: "fas", name: "person-harassing" },
+            { prefix: "fas", name: "brazilian-real-sign" },
+            { prefix: "fas", name: "landmark-alt" },
+            { prefix: "fas", name: "landmark-dome" },
+            { prefix: "fas", name: "arrow-up" },
+            { prefix: "fas", name: "television" },
+            { prefix: "fas", name: "tv-alt" },
+            { prefix: "fas", name: "tv" },
+            { prefix: "fas", name: "shrimp" },
+            { prefix: "fas", name: "tasks" },
+            { prefix: "fas", name: "list-check" },
+            { prefix: "fas", name: "jug-detergent" },
+            { prefix: "fas", name: "user-circle" },
+            { prefix: "fas", name: "circle-user" },
+            { prefix: "fas", name: "user-shield" },
+            { prefix: "fas", name: "wind" },
+            { prefix: "fas", name: "car-crash" },
+            { prefix: "fas", name: "car-burst" },
+            { prefix: "fas", name: "y" },
+            { prefix: "fas", name: "snowboarding" },
+            { prefix: "fas", name: "person-snowboarding" },
+            { prefix: "fas", name: "shipping-fast" },
+            { prefix: "fas", name: "truck-fast" }
+        ]
     }
 };
 
