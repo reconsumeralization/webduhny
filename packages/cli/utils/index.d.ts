ac3f428bcb8db80025e49c955e32ded137873ef2
adrians5j <adrian@webiny.com>
2025-02-15 21:36:12 +0100

    fix: add `sleepSync` types

--- packages/cli/utils/index.d.ts
+++ packages/cli/utils/index.d.ts
@@ -1,4 +1,4 @@
-import {Project, ProjectApplication} from "../types";
+import { Project, ProjectApplication } from "../types";
 
 export interface IGetProjectParams {
     cwd: string;
@@ -10,7 +10,10 @@ export interface IGetProjectApplicationParams {
     cwd: string;
 }
 
-export declare function getProjectApplication(params: IGetProjectApplicationParams): ProjectApplication;
-
+export declare function getProjectApplication(
+    params: IGetProjectApplicationParams
+): ProjectApplication;
 
 export declare function sendEvent(event: string, properties?: Record<string, any>): Promise<void>;
+
+export declare function sleepSync(ms?: number): void;
