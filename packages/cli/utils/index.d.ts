b6f157d3dbd4fb773caf28d95980816db939531a
adrians5j <adrian@webiny.com>
2025-02-17 07:11:25 +0100

    fix: add `log` types

--- packages/cli/utils/index.d.ts
+++ packages/cli/utils/index.d.ts
@@ -17,3 +17,12 @@ export declare function getProjectApplication(
 export declare function sendEvent(event: string, properties?: Record<string, any>): Promise<void>;
 
 export declare function sleepSync(ms?: number): void;
+
+export declare const log: {
+    log: (...args: any[]) => void;
+    info: (...args: any[]) => void;
+    success: (...args: any[]) => void;
+    debug: (...args: any[]) => void;
+    warning: (...args: any[]) => void;
+    error: (...args: any[]) => void;
+};
