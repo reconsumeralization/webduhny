8249eaae4837141d86e42eb73c1378756ba55aaf
adrians5j <adrian@webiny.com>
2025-02-17 06:04:43 +0100

    fix: clean up unused interfaces

--- packages/cli-plugin-deploy-pulumi/src/utils/importStack.ts
+++ packages/cli-plugin-deploy-pulumi/src/utils/importStack.ts
@@ -2,32 +2,6 @@ import { IUserCommandInput } from "~/types";
 import execa from "execa";
 import { getProject } from "@webiny/cli/utils";
 
-interface ILambdaFunctionResourceOutputsCodeAsset {
-    path: string;
-}
-
-interface ILambdaFunctionResource {
-    type: "aws:lambda/function:Function";
-    outputs: {
-        name: string;
-        code: {
-            assets: {
-                [key: string]: ILambdaFunctionResourceOutputsCodeAsset;
-            };
-        };
-    };
-}
-
-export type IImportStackResponseDeploymentResource = ILambdaFunctionResource;
-
-export interface IImportStackResponseDeployment {
-    resources: IImportStackResponseDeploymentResource[];
-}
-
-export interface IImportStackResponse {
-    deployment: IImportStackResponseDeployment;
-}
-
 const runImportStack = ({
     folder,
     env,
@@ -37,31 +11,21 @@ const runImportStack = ({
 }: Pick<IUserCommandInput, "folder" | "env" | "variant" | "cwd" | "file">) => {
     const project = getProject();
 
-    try {
-        const command = ["webiny", "pulumi", folder, "--env", env];
-        if (variant) {
-            command.push("--variant", variant);
-        }
-
-        command.push("--", "stack", "import", "--file", file!);
-
-        const { stdout } = execa.sync("yarn", command.filter(Boolean), {
-            cwd: cwd || project.root
-        });
-
-        const parsed = JSON.parse(stdout);
-        if (Object.keys(parsed).length === 0) {
-            return null;
-        }
-        return parsed;
-    } catch (e) {
-        return null;
+    const command = ["webiny", "pulumi", folder, "--env", env];
+    if (variant) {
+        command.push("--variant", variant);
     }
+
+    command.push("--", "stack", "import", "--file", file!);
+
+    return execa.sync("yarn", command.filter(Boolean), {
+        cwd: cwd || project.root
+    });
 };
 
 export const importStack = (
     args: Pick<IUserCommandInput, "folder" | "env" | "variant" | "cwd" | "file">
-): IImportStackResponse => {
+) => {
     if (!args.folder) {
         throw new Error(`Please specify a project application folder, for example "apps/admin".`);
     }
