7f1eea7197fe190d2852c3cc9593035f8fc44e53
adrians5j <adrian@webiny.com>
2025-03-27 18:16:18 +0100

    wip

--- apps/api/graphql/src/extensions.ts
+++ apps/api/graphql/src/extensions.ts
@@ -1,7 +1,5 @@
 // This file is automatically updated via scaffolding utilities.
-import { createExtension as IntrospectionRemovalExtensionFactory } from "introspection-removal";
-
 // Learn more about extensions: https://webiny.link/extensions
 export const extensions = () => {
-    return [IntrospectionRemovalExtensionFactory()];
+    return [];
 };
