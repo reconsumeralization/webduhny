a06baef6eb1a2c0c8c0de1763bf16e8493d2f464
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-03-28 17:38:59 +0100

    refactor: enhance Drawer component with separator props and adjust layout

--- packages/admin-ui/src/Drawer/components/DrawerFooter.tsx
+++ packages/admin-ui/src/Drawer/components/DrawerFooter.tsx
@@ -1,27 +1,39 @@
 import * as React from "react";
 import { cn } from "~/utils";
+import { Separator } from "~/Separator";
+import type { DrawerProps } from "~/Drawer";
 
 export interface DrawerFooterProps extends React.HTMLAttributes<HTMLDivElement> {
     actions?: React.ReactNode;
     info?: React.ReactNode;
+    separator?: DrawerProps["footerSeparator"];
 }
 
-export const DrawerFooter = ({ actions, info, className, ...props }: DrawerFooterProps) => {
+export const DrawerFooter = ({
+    actions,
+    info,
+    className,
+    separator,
+    ...props
+}: DrawerFooterProps) => {
     if (!actions && !info) {
         return null;
     }
 
     return (
-        <div
-            {...props}
-            className={cn("wby-flex wby-justify-between wby-p-lg wby-pt-md-extra", className)}
-        >
-            {info && (
-                <div className={"wby-text-sm wby-flex wby-items-center"}>
-                    <div>{info}</div>
-                </div>
-            )}
-            {actions && <div className={"wby-flex wby-gap-x-sm wby-ml-auto"}>{actions}</div>}
-        </div>
+        <>
+            {separator && <Separator variant={"dimmed"} margin={"none"} className={"wby-mt-lg"} />}
+            <div
+                {...props}
+                className={cn("wby-flex wby-justify-between wby-p-lg wby-pt-md-extra", className)}
+            >
+                {info && (
+                    <div className={"wby-text-sm wby-flex wby-items-center"}>
+                        <div>{info}</div>
+                    </div>
+                )}
+                {actions && <div className={"wby-flex wby-gap-x-sm wby-ml-auto"}>{actions}</div>}
+            </div>
+        </>
     );
 };
