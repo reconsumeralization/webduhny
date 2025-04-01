4d72040116d88891189f09988b1be1961311d419
adrians5j <adrian@webiny.com>
2025-04-01 06:53:42 +0200

    wip

--- packages/app-admin-ui/src/Navigation/Navigation.tsx
+++ packages/app-admin-ui/src/Navigation/Navigation.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { NavigationRenderer, useAdminConfig } from "@webiny/app-admin";
 import { Sidebar } from "@webiny/admin-ui";
 import { SidebarMenuItems } from "./SidebarMenuItems";
+import { Link } from "@webiny/react-router";
 
 export const Navigation = NavigationRenderer.createDecorator(() => {
     return function Navigation() {
         const { menus, tenant } = useAdminConfig();
 
         const title = tenant.name;
-        const icon = <Sidebar.Icon element={tenant.logo} label={"Webiny"} />;
+        const icon = (
+            <Link to={"/"}>
+                <Sidebar.Icon element={tenant.logo} label={"Webiny"} />
+            </Link>
+        );
 
         return (
             <Sidebar
