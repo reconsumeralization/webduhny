f36a0ef6f2733952af1a5c51fd01cbe2d2a71754
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-04-03 08:16:22 +0200

    refactor: adjust max height for preview component to improve layout

--- packages/app-file-manager/src/components/FileDetails/components/Preview.tsx
+++ packages/app-file-manager/src/components/FileDetails/components/Preview.tsx
@@ -9,7 +9,7 @@ export const Preview = () => {
                 "wby-h-full wby-w-auto wby-flex wby-items-center wby-justify-center",
                 "wby-bg-[repeating-conic-gradient(theme(backgroundColor.neutral.dimmed)_0%_25%,transparent_0%_50%)]",
                 "wby-bg-[length:24px_24px] wby-bg-center",
-                "[&_*]:wby-max-w-[400px] [&_*]:wby-max-h-[calc(100vh-226px)]"
+                "[&_*]:wby-max-w-[400px] [&_*]:wby-max-h-[calc(100vh-274px)]"
             )}
         >
             <Thumbnail />
