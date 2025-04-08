6cfe7eab260ae7511f20a74dabfc85bb487ccda7
adrians5j <adrian@webiny.com>
2025-04-08 06:55:44 +0200

    wip

--- packages/admin-ui/tailwind.config.js
+++ packages/admin-ui/tailwind.config.js
@@ -1,10 +1,5 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
-const { getProject } = require("@webiny/cli/utils");
-const project = getProject();
-const webinyPackagesGlob = `${project.root}/node_modules/@webiny/app*/**/*.{js,ts,tsx}`;
-const webinyAdminUiPackageGlob = `${project.root}/node_modules/@webiny/admin-ui/**/*.{js,ts,tsx}`;
-const webinyUiPackageGlob = `${project.root}/node_modules/@webiny/ui/**/*.{js,ts,tsx}`;
-const adminAppSourceGlob = `${project.root}/apps/admin`;
+const webinyPackagesGlob = `src/**/*.{js,ts,tsx}`;
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,9 +7,6 @@ module.exports = {
     darkMode: ["class"],
     content: [
         webinyPackagesGlob,
-        webinyAdminUiPackageGlob,
-        webinyUiPackageGlob,
-        adminAppSourceGlob
     ],
     theme: {
         container: {
