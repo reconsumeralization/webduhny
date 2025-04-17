73485c840077cb8583474b575f019e02b8d5b237
adrians5j <adrian@webiny.com>
2025-04-17 06:15:49 +0200

    wip

--- extensions/funnelBuilder/src/frontend/pageElements/container/website/ContainerWebsite.tsx
+++ extensions/funnelBuilder/src/frontend/pageElements/container/website/ContainerWebsite.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { createRenderer, useRenderer, Elements } from "@webiny/app-page-builder-elements";
 import { Form } from "@webiny/form";
-import { ContainerProvider } from "../ContainerContext";
 
 export const ContainerWebsite = createRenderer(() => {
     const { getElement } = useRenderer();
     const element = getElement();
 
-    console.log('renderiram WEBSITE ❗❗❗❗')
+    console.log("renderiram WEBSITE ❗❗❗❗");
     return (
         <div>
-            <ContainerProvider>
-                <Form
-                    onSubmit={data => {
-                        console.log("Form submitted");
-                        console.log("dejta", data);
-                    }}
-                >
-                    {() => <Elements element={element} />}
-                </Form>
-            </ContainerProvider>
+            <Form
+                onSubmit={data => {
+                    console.log("Form submitted");
+                    console.log("dejta", data);
+                }}
+            >
+                {() => <Elements element={element} />}
+            </Form>
         </div>
     );
 });
--- extensions/funnelBuilder/src/website.tsx
+++ extensions/funnelBuilder/src/website.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ContainerWebsitePlugins } from "./frontend/pageElements/container/website/ContainerWebsitePlugins";
-import { InputWebsite } from "./frontend/pageElements/formElements/input/website";
-import { ButtonWebsite } from "./frontend/pageElements/formElements/button/website";
+// import { InputWebsite } from "./frontend/pageElements/formElements/input/website";
+// import { ButtonWebsite } from "./frontend/pageElements/formElements/button/website";
 
 export const Extension = () => (
     <>
         <ContainerWebsitePlugins />
 
         {/* Form Elements */}
-        <InputWebsite />
-        <ButtonWebsite />
+        {/*<InputWebsite />*/}
+        {/*<ButtonWebsite />*/}
     </>
 );
