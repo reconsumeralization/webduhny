9e094d93ee5938a5dc46ddf771017c2800937c41
adrians5j <adrian@webiny.com>
2025-04-25 10:16:20 +0200

    wip

---
+++ extensions/funnelBuilder/src/frontend/admin/plugins/PbEditorFunnelFieldPageElementPlugin.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { PbEditorPageElementPlugin } from "@webiny/app-page-builder";
+import { OnCreateActions } from "@webiny/app-page-builder/types";
+import { ReactComponent as TextIcon } from "@material-design-icons/svg/outlined/text_fields.svg";
+import { Typography } from "@webiny/ui/Typography";
+import styled from "@emotion/styled";
+import type { Renderer } from "@webiny/app-page-builder-elements/types";
+import { createFieldElementType } from "../../../shared/constants";
+import { createInitialFieldData, FUB_PAGE_ELEMENT_GROUP } from "../../pageElements/fields/utils";
+
+const StyledPreview = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
+`;
+
+export interface PbEditorFunnelFieldPageElementPluginProps {
+    fieldType: string;
+    renderer: Renderer;
+    name: string;
+    description: string;
+    extraFieldData: Record<string, any>;
+}
+
+export const PbEditorFunnelFieldPageElementPlugin = (
+    props: PbEditorFunnelFieldPageElementPluginProps
+) => {
+    const fieldType = props.fieldType;
+    const pbElementType = createFieldElementType(fieldType);
+    return (
+        <PbEditorPageElementPlugin
+            elementType={pbElementType}
+            renderer={props.renderer}
+            toolbar={{
+                title: props.name,
+                group: FUB_PAGE_ELEMENT_GROUP,
+                preview() {
+                    return (
+                        <StyledPreview>
+                            <TextIcon />
+                            <Typography use={"headline6"}>{props.name}</Typography>
+                            <Typography use={"body2"}>{props.description}</Typography>
+                        </StyledPreview>
+                    );
+                }
+            }}
+            // Defines which types of element settings are available to the user.
+            settings={[
+                "pb-editor-page-element-settings-delete",
+                "pb-editor-page-element-settings-visibility",
+                "pb-editor-page-element-style-settings-padding",
+                "pb-editor-page-element-style-settings-margin",
+                "pb-editor-page-element-style-settings-width",
+                "pb-editor-page-element-style-settings-height",
+                "pb-editor-page-element-style-settings-background"
+            ]}
+            target={["cell", "block"]}
+            onCreate={OnCreateActions.OPEN_SETTINGS}
+            // `create` function creates the initial data for the page element.
+            create={options => {
+                const extraFieldData = structuredClone(props.extraFieldData);
+                return {
+                    type: pbElementType,
+                    elements: [],
+                    data: createInitialFieldData(fieldType, extraFieldData),
+                    ...options
+                };
+            }}
+        />
+    );
+};
--- extensions/funnelBuilder/src/frontend/pageElements/fields/text/TextFieldAdminPlugins.tsx
+++ extensions/funnelBuilder/src/frontend/pageElements/fields/text/TextFieldAdminPlugins.tsx
@@ -1,61 +1,27 @@
 import React from "react";
-import { PbEditorPageElementPlugin, PbRenderElementPlugin } from "@webiny/app-page-builder";
+import { PbRenderElementPlugin } from "@webiny/app-page-builder";
 import { TextFieldRenderer } from "./TextFieldRenderer";
-import { OnCreateActions } from "@webiny/app-page-builder/types";
 import { ELEMENT_TYPE } from "./constants";
-import { createInitialFieldData, FUB_PAGE_ELEMENT_GROUP } from "../utils";
 import { TextFieldSettings } from "./TextFieldSettings";
 import { PbEditorFunnelFieldSettingsPlugin } from "../../../admin/plugins/PbEditorFunnelFieldSettingsPlugin";
-import { ReactComponent as TextIcon } from "@material-design-icons/svg/outlined/text_fields.svg";
+import { PbEditorFunnelFieldPageElementPlugin } from "../../../admin/plugins/PbEditorFunnelFieldPageElementPlugin";
 
 export const TextFieldAdminPlugins = () => {
     return (
         <>
             <PbRenderElementPlugin elementType={ELEMENT_TYPE} renderer={TextFieldRenderer} />
-            <PbEditorPageElementPlugin
-                elementType={ELEMENT_TYPE}
+            <PbEditorFunnelFieldPageElementPlugin
+                fieldType={"text"}
                 renderer={TextFieldRenderer}
-                toolbar={{
-                    title: "Short Text",
-                    group: FUB_PAGE_ELEMENT_GROUP,
-                    preview() {
-                        return <TextIcon/>
-                         // return <>Titles, names, single line input</>;
-                    }
-                }}
-                // Defines which types of element settings are available to the user.
-                settings={[
-                    "pb-editor-page-element-settings-delete",
-                    "pb-editor-page-element-settings-visibility",
-                    "pb-editor-page-element-style-settings-padding",
-                    "pb-editor-page-element-style-settings-margin",
-                    "pb-editor-page-element-style-settings-width",
-                    "pb-editor-page-element-style-settings-height",
-                    "pb-editor-page-element-style-settings-background"
-                ]}
-                // Defines onto which existing elements our element can be dropped.
-                // In most cases, using `["cell", "block"]` will suffice.
-                target={["cell", "block"]}
-                onCreate={OnCreateActions.OPEN_SETTINGS}
-                // `create` function creates the initial data for the page element.
-                create={options => {
-                    return {
-                        type: ELEMENT_TYPE,
-                        elements: [],
-                        data: createInitialFieldData("text", {
-                            inputType: "",
-                            placeholderText: ""
-                        }),
-                        ...options
-                    };
+                name={"Short Text"}
+                description={"Titles, names, single line input"}
+                extraFieldData={{
+                    inputType: "",
+                    placeholderText: ""
                 }}
             />
 
-            <PbEditorFunnelFieldSettingsPlugin
-                fieldType={"text"}
-                // validators={["required", "minLength", "maxLength", "pattern"]}
-                renderer={TextFieldSettings}
-            />
+            <PbEditorFunnelFieldSettingsPlugin fieldType={"text"} renderer={TextFieldSettings} />
         </>
     );
 };
