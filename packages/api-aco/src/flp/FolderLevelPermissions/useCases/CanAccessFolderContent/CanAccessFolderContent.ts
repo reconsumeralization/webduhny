69ef7af2f1ea820c224f236cf0e6b30f51300779
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-05-12 17:08:13 +0200

    feat(api-aco): add no-access level and allow editor to rwd folders

--- packages/api-aco/src/flp/FolderLevelPermissions/useCases/CanAccessFolderContent/CanAccessFolderContent.ts
+++ packages/api-aco/src/flp/FolderLevelPermissions/useCases/CanAccessFolderContent/CanAccessFolderContent.ts
@@ -22,10 +22,16 @@ export class CanAccessFolderContent implements ICanAccessFolderContent {
             return false;
         }
 
+        const { level } = currentIdentityPermission;
+
+        // If the user has a `no-access` level, they are explicitly denied access to the current folder.
+        if (level === "no-access") {
+            return false;
+        }
+
         // If the user is not an owner and we're checking for "write" or
         // "delete" access, then we can immediately return false.
         if (rwd !== "r") {
-            const { level } = currentIdentityPermission;
             return level !== "viewer";
         }
 
