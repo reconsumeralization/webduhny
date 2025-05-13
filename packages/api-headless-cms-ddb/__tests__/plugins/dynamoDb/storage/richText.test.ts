3d01ed16b3cd133b6d8e564f0c72e536ebf363d9
Bruno Zorić <bruno.zoric@gmail.com>
2025-05-13 09:14:37 +0200

    fix(api-headless-cms): tests

--- packages/api-headless-cms-ddb/__tests__/plugins/dynamoDb/storage/richText.test.ts
+++ packages/api-headless-cms-ddb/__tests__/plugins/dynamoDb/storage/richText.test.ts
@@ -84,4 +84,20 @@ describe("richTextStoragePlugin", () => {
 
         expect(result).toEqual(testValue);
     });
+
+    it("fromStorage should transform value for output - jsonpack", async () => {
+        const value = {
+            compression: "jsonpack",
+            value: "h1|big+title|h2|small+title|h3|subtitle|p|subtitle+description|number919|boolTrue|boolFalse|textYes|yes|content|start+of+the+text|a|text|url|href|https://www.webiny.com|end+of+the+text|more+url|target|_blank^PJ^^@$0|1|2|3]|$4|5|6|7]|$8|O|9|-1|A|-2|B|C|D|@$6|E|F|$G|H|I|J]]|$6|K|F|$G|L|I|J|M|N]]]]]"
+        };
+        const plugin = createRichTextStorageTransformPlugin();
+
+        const result = await plugin.fromStorage({
+            ...defaultArgs,
+            value,
+            plugins: context.plugins
+        } as unknown as FromStorageParams<any, any>);
+
+        expect(result).toEqual(testValue);
+    });
 });
