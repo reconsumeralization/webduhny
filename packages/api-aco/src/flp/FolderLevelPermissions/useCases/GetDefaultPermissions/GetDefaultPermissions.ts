e9760057c690cc140630fb79be9fed881303305d
adrians5j <adrian@webiny.com>
2025-05-19 07:19:23 +0200

    fix: extract permissions merging

--- packages/api-aco/src/flp/FolderLevelPermissions/useCases/GetDefaultPermissions/GetDefaultPermissions.ts
+++ packages/api-aco/src/flp/FolderLevelPermissions/useCases/GetDefaultPermissions/GetDefaultPermissions.ts
@@ -1,6 +1,7 @@
 import type { IGetDefaultPermissions } from "./IGetDefaultPermissions";
 import type { IGetIdentityGateway, IListPermissionsGateway } from "../../gateways";
-import type { FolderAccessLevel, FolderPermission } from "~/flp/flp.types";
+import type { FolderPermission } from "~/flp/flp.types";
+import { DefaultPermissionsMerger } from "./DefaultPermissionsMerger";
 
 export class GetDefaultPermissions implements IGetDefaultPermissions {
     private getIdentityGateway: IGetIdentityGateway;
@@ -15,47 +16,9 @@ export class GetDefaultPermissions implements IGetDefaultPermissions {
     }
 
     async execute(permissions: FolderPermission[]) {
-        const hasFullAccess = await this.hasFullAccess();
         const identity = this.getIdentityGateway.execute();
+        const identityPermissions = await this.listPermissionsGateway.execute();
 
-        /**
-         * If the user has full access to the application, add a specific "owner" permission to the list.
-         * This ensures the user has complete control over the folder.
-         */
-        if (hasFullAccess) {
-            return [
-                {
-                    target: `admin:${identity.id}`,
-                    level: "owner" as FolderAccessLevel,
-                    inheritedFrom: "role:full-access"
-                },
-                /**
-                 * Remove any permissions related to the full access user,
-                 * as these are always superseded by the "owner" permission defined above.
-                 */
-                ...permissions.filter(p => p.target !== `admin:${identity.id}`)
-            ];
-        }
-
-        if (permissions.length > 0) {
-            return permissions;
-        }
-
-        /**
-         * No permissions provided. This means the folder is public.
-         * Add a specific "public" permission to the list to ensure the folder is accessible to everyone.
-         */
-        return [
-            {
-                target: `admin:${identity.id}`,
-                level: "public" as FolderAccessLevel,
-                inheritedFrom: "public"
-            }
-        ];
-    }
-
-    private async hasFullAccess(): Promise<boolean> {
-        const permissions = await this.listPermissionsGateway.execute();
-        return permissions.some(p => p.name === "*");
+        return DefaultPermissionsMerger.merge(identity, identityPermissions, permissions);
     }
 }
