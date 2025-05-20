2fb3985295a6b685eca794cd875585e8eca8f45c
adrians5j <adrian@webiny.com>
2025-05-20 16:28:13 +0200

    wip

--- apps/api/graphql/src/extensions.ts
+++ apps/api/graphql/src/extensions.ts
@@ -1,5 +1,7 @@
 // This file is automatically updated via scaffolding utilities.
+import { createExtension as MigrationExtensionFactory } from "migration";
+
 // Learn more about extensions: https://webiny.link/extensions
 export const extensions = () => {
-    return [];
+    return [MigrationExtensionFactory()];
 };
