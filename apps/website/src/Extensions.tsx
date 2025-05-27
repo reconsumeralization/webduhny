5a7eeeadab8387b39bcfed5df83cca84d7009894
adrians5j <adrian@webiny.com>
2025-05-27 09:12:35 +0200

    wip

--- apps/website/src/Extensions.tsx
+++ apps/website/src/Extensions.tsx
@@ -1,12 +1,9 @@
 // This file is automatically updated via scaffolding utilities.
 // Learn more about extensions: https://webiny.link/extensions
 import React from "react";
-import { Extension as PageElementsExtension } from "page-elements/src/website";
 
 export const Extensions = () => {
     return (
-        <>
-            <PageElementsExtension />
-        </>
+        <></>
     );
 };
