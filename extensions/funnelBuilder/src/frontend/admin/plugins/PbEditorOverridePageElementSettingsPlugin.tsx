289549a6943f31da7373239ac9b53cf214b4de70
adrians5j <adrian@webiny.com>
2025-06-05 11:19:29 +0200

    fix: override page el settings

--- extensions/funnelBuilder/src/frontend/admin/plugins/PbEditorOverridePageElementSettingsPlugin.tsx
+++ extensions/funnelBuilder/src/frontend/admin/plugins/PbEditorOverridePageElementSettingsPlugin.tsx
@@ -31,7 +31,7 @@ export const PbEditorOverridePageElementSettingsPlugin = (
                 return {
                     ...plugin,
                     ...props
-                } as T;
+                };
             });
         };
 
