47630677c39a80f5bcc784c634ebb551e5b5aba1
Leonardo Giacone <leonardo.giacone@gmail.com>
2025-06-07 15:26:42 +0200

    fix: refactor Sidebar component into subcomponents

---
+++ packages/app-headless-cms/src/admin/components/ContentEntries/SidebarFooter/SidebarFooter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TrashBin } from "~/admin/components/ContentEntries/TrashBin";
+
+export const SidebarFooter = () => {
+    return (
+        <div className={"wby-px-xs wby-py-sm wby-bg-neutral-base"}>
+            <TrashBin />
+        </div>
+    );
+};
